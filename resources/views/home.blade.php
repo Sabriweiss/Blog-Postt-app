da8eab58efdde132351a3b32d572e8fe73f3fb6b
Sabri Weisse <126602101+Sabriweiss@users.noreply.github.com>
2025-07-16 10:53:29 +0300

    deleted unnecessary files

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Blog Page</title>
+</head>
+
+<body>
+    <h1>Welcome to the Home Page</h1>
+    <p>This is the home page of the blog post application.</p>
+    <a href="{{ route('home') }}">Go to Home</a>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,54 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/home', function () {
+    return view('home');
+})->name('home');
+
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
+
+Route::get('/services', function () {
+    return view('services');
+})->name('services');
+
+Route::get('/blog', function () {
+    return view('blog');
+})->name('blog');
+
+Route::get('/blog/{id}', function ($id) {
+    return view('blog-post', ['id' => $id]);
+})->name('blog.post');
+
+Route::get('/portfolio', function () {
+    return view('portfolio');
+})->name('portfolio');
+Route::get('/portfolio/{id}', function ($id) {
+    return view('portfolio-item', ['id' => $id]);
+})->name('portfolio.item');
+Route::get('/faq', function () {
+    return view('faq');
+})->name('faq');
+
+Route::get('/terms', function () {
+    return view('terms');
+})->name('terms');
+
+Route::get('/privacy', function () {
+    return view('privacy');
+})->name('privacy');
+
+Route::get('/login', function () {
+    return view('auth.login');
+})->name('login');
+
+Route::get('/register', function () {
+    return view('auth.register');
+})->name('register');
